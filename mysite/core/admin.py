3cddf734bb9a2db23972fc1b2e697082c504b1d2
Sreejeeth <sreejeeth.ramprasad1996@gmail.com>
2017-10-26 22:02:39 +0530

    first commit

---
+++ mysite/core/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from mysite.core.models import Productdb,Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug','category','image','price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available','image', 'created', 'updated',]
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Productdb, ProductAdmin)
